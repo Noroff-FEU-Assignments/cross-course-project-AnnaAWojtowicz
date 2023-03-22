b8c45f589b4fec9fe64abd9ebbd3b4b5be11c285
Anna Wojtowicz <annwoj26185@stud.noroff.no>
2023-03-22 19:56:36 +0100

    js part2

--- js/details.js
+++ js/details.js
@@ -18,24 +18,95 @@ async function getFilmDetails(id) {
     return responseBody;
 }
 
+async function getFilmCredits(id) {
+    let responseBody;
+    try {
+        const response = await fetch(getFilmCreditsUrl(id));
+        responseBody = await response.json();
+    }
+    catch (error) {
+        console.log("This does not work", error);
+        detailResultsContainer.innerHTML = "An error has occured! Please try again later. Bye.";
+    }
+    return responseBody;
+}
 
-function createDetailsResultsHtml(movie) {
-    detailResultsContainer.innerHTML = `
-  <section class="container">
-  <div class="titles" id="title">${movie.title}</div>
-  <img src="${imageUrl}/${movie.backdrop_path}" class="img">
-
-<div class="genre">Genre: ${movie.genre}</div>
-
-
-  <div class="release__date">Release date: ${movie.release_date.substring(0, 4)}</div>
- 
-
-  <div class="time">Runtime: ${movie.runtime}</div>
-  <div class="overview">Overview: ${movie.overview}</div>
-  
-  </section>
-  `;
+function getDirector(movieDetails) {
+    const crew = movieDetails.crew;
+    let resultSring = "";
+    for (let i = 0; i < crew.length; i++) {
+        if (crew[i].job === "Director") {
+            resultSring = crew[i].name;
+            break;
+        }
+    }
+    return resultSring;
 }
 
 
+
+function getGenre(movieDetails) {
+    const genres = movieDetails.genres;
+    let resultString = "";
+    for (let i = 0; i < genres.length; i++) {
+        resultString += genres[i].name + " ";
+    }
+    return resultString;
+}
+
+function getProdCount(movieDetails) {
+    const prodCount = movieDetails.production_countries;
+    let resultString = "";
+    for (let i = 0; i < prodCount.length; i++) {
+        resultString += prodCount[i].name + " ";
+    }
+    return resultString;
+}
+
+
+function createDetailsResultsHtml(movie, credits) {
+    detailResultsContainer.innerHTML = `
+    <div class="title-pic-film-info-and-cta">
+    <div class="title-pic">
+        <h1 class="title__my-films">${movie.title}</h1>
+    
+    
+        <div class="film-window">
+        <img src="${imageUrl}/${movie.backdrop_path}" class="main-style__pic-my-films" />
+            <section class="icons-in-the-film">
+    
+                <div class="material-symbols-rounded info"><a href="#">shopping_basket</a></div>
+                <div class="material-symbols-rounded favorite"><a href="#">favorite</a></div>
+    
+            </section>
+        </div>
+    </div>
+
+    <div class="film-info-and-cta">
+        <div class="film-info">
+            <h3>Genre: ${getGenre(movie)}</h3>
+            <h3>Directed by: ${getDirector(credits)}</h3>
+            <div class="country-year-min">
+                <h3>${getProdCount(movie)}, ${movie.release_date.substring(0, 4)}</h3>
+                <h3>${movie.runtime}min</h3>
+            </div>
+            <p>
+            ${movie.overview}
+            </p>
+        </div>
+        <div class="cta">
+            <a href="../index2.html">go back</a>
+        </div>
+    </div>
+    </div>
+  `;
+}
+
+async function run(id) {
+    let movie = await getFilmDetails(id);
+    let credits = await getFilmCredits(id);
+    createDetailsResultsHtml(movie, credits);
+
+}
+
+run(id);
--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
-const resultsContainer = document.querySelector(".results");
+const resultsContainerTodaysFilm = document.getElementById("pic-todays-film");
+const resultsContainerNewMovies = document.getElementById("newMovies");
+const resultsContainerDrama = document.getElementById("drama");
+const resultsContainerDoc = document.getElementById("doc");
+const resultsContainerJap = document.getElementById("jap");
 
+
+console.log(resultsContainerNewMovies);
 
 async function getResults(tmdbUrl) {
     try {
@@ -9,11 +15,11 @@ async function getResults(tmdbUrl) {
     }
     catch (error) {
         console.log("This does not work", error);
-        resultsContainer.innerHTML = "An error has occured! Please try again later. Bye.";
+        alert("An error has occured! Please try again later. Bye.");
     }
 }
 
-async function getPopularMovie() {
+async function getTodaysFilm() {
     const poplarMovies = await getResults(getPopularUrl);
     return poplarMovies[0];
 }
@@ -29,28 +35,132 @@ async function getDrama() {
     return drama;
 }
 
-async function getJapanese() {
+async function getDoc() {
+    const doc = await getResults(getDocUrl);
+    return doc;
+}
+
+async function getJap() {
     const japanese = await getResults(getJapaneseUrl);
     return japanese;
 }
 
-async function test2() {
-    const test = await getJapanese();
-    console.log(test);
+// async function test2() {
+//     const test = await getJapanese();
+//     console.log(test);
+// }
+
+// test2();
+
+// function createGetPopularHtml(movies) {
+//     for (let i = 0; i < movies.length; i++) {
+//         resultsContainer.innerHTML += `
+//     <section class="container">
+//     <a href="/details.html?id=${movies[i].id}" class="title"><div class="titles" id="title">${movies[i].title}</div>
+//     <img src="${imageUrl}/${movies[i].backdrop_path}" class="img">
+//     <div class="release__date">Release date: ${movies[i].release_date.substring(0, 4)}</div>
+//     <div class="score">Score: ${movies[i].vote_average}</div></a>
+//     </section>
+//     `;
+//     }
+// }
+
+function createTodaysFilmHtml(movie) {
+    resultsContainerTodaysFilm.style.backgroundImage =
+        "url('https://image.tmdb.org/t/p/w500//2Eewgp7o5AU1xCataDmiIL2nYxd.jpg'), linear-gradient(180deg, rgba(13, 13, 13, 0) 80%, #0D0D0D 100%), linear-gradient(180deg, #000000, rgba(13, 13, 13, 0) 30%)";
+    // resultsContainerTodaysFilm.innerHTML += `
+    // <div class="film-window">
+    //       <h1 class="todays-film__h1 todays-film__logged-in>Today's Film: ${movie.title}</h1>
+    //      <img src="${imageUrl}/${movie.backdrop_path}" class="film__carousel__general all-pic-carousel pic-carousel">
+    //       <section class="icons-in-the-film">
+    //         <div class="material-symbols-rounded info"><a href="/details.html?id=${movie.id}">info</a></div>
+    //         <div class="material-symbols-rounded shoping-basket"><a href="#">shopping_basket</a></div>
+    //         <div class="material-symbols-rounded favorite"><a href="#">favorite</a></div>
+    //       </section>
+    // </div>
+    // `;
+
 }
 
-test2();
-
-function createGetPopularHtml(movies) {
+function createGetNowPlayingHtml(movies) {
     for (let i = 0; i < movies.length; i++) {
-        resultsContainer.innerHTML += `
-    <section class="container">
-    <a href="/details.html?id=${movies[i].id}" class="title"><div class="titles" id="title">${movies[i].title}</div>
-    <img src="${imageUrl}/${movies[i].backdrop_path}" class="img">
-    <div class="release__date">Release date: ${movies[i].release_date.substring(0, 4)}</div>
-    <div class="score">Score: ${movies[i].vote_average}</div></a>
-    </section>
+        resultsContainerNewMovies.innerHTML += `
+    <div class="film-window">
+          <h1 class="h1-in-the-film">${movies[i].title}</h1>
+         <img src="${imageUrl}/${movies[i].backdrop_path}" class="film__carousel__general all-pic-carousel pic-carousel">
+          <section class="icons-in-the-film">
+            <div class="material-symbols-rounded info"><a href="/details.html?id=${movies[i].id}">info</a></div>
+            <div class="material-symbols-rounded shoping-basket"><a href="#">shopping_basket</a></div>
+            <div class="material-symbols-rounded favorite"><a href="#">favorite</a></div>
+          </section>
+    </div>
     `;
     }
 }
 
+
+
+
+function createGetDramaHtml(movies) {
+    for (let i = 0; i < movies.length; i++) {
+        resultsContainerDrama.innerHTML += `
+    <div class="film-window">
+          <h1 class="h1-in-the-film">${movies[i].title}</h1>
+         <img src="${imageUrl}/${movies[i].backdrop_path}" class="film__carousel__general all-pic-carousel pic-carousel">
+          <section class="icons-in-the-film">
+            <div class="material-symbols-rounded info"><a href="/details.html?id=${movies[i].id}">info</a></div>
+            <div class="material-symbols-rounded shoping-basket"><a href="#">shopping_basket</a></div>
+            <div class="material-symbols-rounded favorite"><a href="#">favorite</a></div>
+          </section>
+    </div>
+    `;
+    }
+}
+
+function createGetDocHtml(movies) {
+    for (let i = 0; i < movies.length; i++) {
+        resultsContainerDoc.innerHTML += `
+    <div class="film-window">
+          <h1 class="h1-in-the-film">${movies[i].title}</h1>
+         <img src="${imageUrl}/${movies[i].backdrop_path}" class="film__carousel__general all-pic-carousel pic-carousel">
+          <section class="icons-in-the-film">
+            <div class="material-symbols-rounded info"><a href="/details.html?id=${movies[i].id}">info</a></div>
+            <div class="material-symbols-rounded shoping-basket"><a href="#">shopping_basket</a></div>
+            <div class="material-symbols-rounded favorite"><a href="#">favorite</a></div>
+          </section>
+    </div>
+    `;
+    }
+}
+
+function createGetJapHtml(movies) {
+    for (let i = 0; i < movies.length; i++) {
+        resultsContainerJap.innerHTML += `
+    <div class="film-window">
+          <h1 class="h1-in-the-film">${movies[i].title}</h1>
+         <img src="${imageUrl}/${movies[i].backdrop_path}" class="film__carousel__general all-pic-carousel pic-carousel">
+          <section class="icons-in-the-film">
+            <div class="material-symbols-rounded info"><a href="/details.html?id=${movies[i].id}">info</a></div>
+            <div class="material-symbols-rounded shoping-basket"><a href="#">shopping_basket</a></div>
+            <div class="material-symbols-rounded favorite"><a href="#">favorite</a></div>
+          </section>
+    </div>
+    `;
+    }
+}
+
+
+async function run() {
+    const moviesTodaysFilm = await getTodaysFilm();
+    createTodaysFilmHtml(moviesTodaysFilm);
+    const moviesNowPlaying = await getNowPlaying();
+    createGetNowPlayingHtml(moviesNowPlaying);
+    const moviesGetDrama = await getDrama();
+    createGetDramaHtml(moviesGetDrama);
+    const moviesGetDoc = await getDoc();
+    createGetDocHtml(moviesGetDoc);
+    const moviesGetJap = await getJap();
+    createGetJapHtml(moviesGetJap);
+}
+
+run();
